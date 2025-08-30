024ef0c1201fffd905b16121841342da8c784401
Lautaro Anriquez <lautaroanriquez.la@gmail.com>
2025-08-30 00:51:29 -0300

    implementando .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLpvmA9NEqlelOo_BpL_9IwV1okFz9RqQ",
-  authDomain: "proyecto-reactjs-7ba1c.firebaseapp.com",
-  projectId: "proyecto-reactjs-7ba1c",
-  storageBucket: "proyecto-reactjs-7ba1c.firebasestorage.app",
-  messagingSenderId: "915026002746",
-  appId: "1:915026002746:web:ba070bf0bc9b1239eec603"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APPID
 };
 
 
